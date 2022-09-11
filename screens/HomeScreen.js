5d641214155847120160c0946708ef7b878ff38e
Kovacs Peter <kovacs2013peter@gmail.com>
2022-09-11 18:26:30 +0200

    Heroicons installed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
 import { TailwindProvider } from 'tailwind-rn/dist';
+import { useNavigation } from '@react-navigation/native';
+
+
+
 const HomeScreen = () => {
+
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+
+        navigation.setOptions({
+            //headerTitle:"Teszt"
+            headerShown: false,
+        })
+    
+    }, [])
+
   return (
 
-    <TailwindProvider>
+   <SafeAreaView>
     <View>
       <Text className="text-red-500">HomeScreen</Text>
+
+    <View>
+        <Image
+            source = {{
+                uri:'https://links.papareact.com/wru'
+            }}
+            className = 'h-7 w-7 bg-gray-300 rounded-full'
+        />
     </View>
-    </TailwindProvider>
+
+
+    <View>
+        <Text className="font-bold text-gray-400 text-xs">Szállítás most</Text>
+        <Text className="font-bold text-xl">Előző lokáció</Text>
+    </View>
+
+
+    </View>
+    </SafeAreaView>
   )
 }
 
